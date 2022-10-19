28f8467b94baf94efc830b63b34d87ae24780a5d
Alex Pitzel <pitzelalex@gmail.com>
2022-10-19 13:25:42 -0400

    feat: Make Player pass 'has a nickname' test

--- lib/player.rb
+++ lib/player.rb
@@ -1,5 +1,5 @@
 class Player
-  attr_reader :first_name, :last_name, :monthly_cost, :contract_length, :total_cost
+  attr_reader :first_name, :last_name, :monthly_cost, :contract_length, :total_cost, :nickname
 
   def initialize(name,cost,length)
     @first_name = name.split.first
@@ -7,5 +7,6 @@ class Player
     @monthly_cost = cost
     @contract_length = length
     @total_cost = cost * length
+    @nickname = nil
   end
 end
