5c7d4508b8b830765e247c0c720c2e12368b1127
Alex Pitzel <pitzelalex@gmail.com>
2022-10-19 13:21:35 -0400

    feat: Make Player pass 'has a monthly cost' test

--- lib/player.rb
+++ lib/player.rb
@@ -1,10 +1,10 @@
 class Player
-  attr_reader :first_name, :last_name, :cost, :length
+  attr_reader :first_name, :last_name, :monthly_cost, :length
 
   def initialize(name,cost,length)
     @first_name = name.split.first
     @last_name = name.split.last
-    @cost = cost
+    @monthly_cost = cost
     @length = length
   end
 end
